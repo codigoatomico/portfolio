a2089ad89fc2e5656982de7788a37405f1caaf56
Juan Pablo Hurtado <juanhurtado.codigoatomico@gmail.com>
2021-04-09 21:45:39 -0500

    feat(Images): Featured Images

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -2,6 +2,7 @@ import { graphql, Link } from 'gatsby'
 import React from "react"
 import Layout from "../../components/Layout"
 import styles from "../../styles/projects.module.css"
+import Img from "gatsby-image"
 
 export default function Projects({ data }) {
   console.log(data)
@@ -9,7 +10,6 @@ export default function Projects({ data }) {
   const contact = data.contact.siteMetadata.contact
   return (
     <Layout>
-      Hello
       <div className={styles.portfolio}>
           <h2>Portfolio</h2>
           <h3>Projets & Websites I've Created</h3>
@@ -17,6 +17,7 @@ export default function Projects({ data }) {
             {projects.map(project => (
               <Link to={"/projects/" + project.frontmatter.slug} key={project.id}>
                 <div>
+                <Img fluid={project.frontmatter.thumb.childImageSharp.fluid} />
                   <h3>{ project.frontmatter.title }</h3>
                   <p>{ project.frontmatter.stack }</p>
                 </div>
@@ -37,6 +38,13 @@ query ProjectsPage {
         title
         slug
         stack
+        thumb {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
       }
       id
     }
@@ -47,5 +55,6 @@ query ProjectsPage {
     }
   }
 }
+
 
 `
